373b4ffa19cdd546bf60ad0853ea4fb63045e625
jh.kim <fnqllove11@naver.com>
2022-09-04 21:13:38 +0900

    로그아웃 기능 추가

--- mood-myapp/src/main/java/mood/moodmyapp/controller/LoginController.java
+++ mood-myapp/src/main/java/mood/moodmyapp/controller/LoginController.java
@@ -2,6 +2,7 @@ package mood.moodmyapp.controller;
 
 import mood.moodmyapp.domain.Member;
 import mood.moodmyapp.service.LoginService;
+import mood.moodmyapp.session.SessionConstant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +11,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.enterprise.inject.Model;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
-@RequestMapping(value="/login")
 public class LoginController {
 
     @Autowired
@@ -22,19 +24,35 @@ public class LoginController {
         this.loginService = loginService;
     }
 
-    @GetMapping("/login.do")
+    @GetMapping("/login/login.do")
     public String loginForm(){
         return "/login/login";
     }
 
 
-    @PostMapping("/login.do")
-    public String loginProc(@ModelAttribute Member member){
+    @PostMapping("/login/login.do")
+    public String loginProc(@ModelAttribute Member member, HttpServletRequest request){
 
+        // 로그인 성공
         if(loginService.login(member)){
+            boolean loginMember = loginService.login(member);
+            HttpSession session = request.getSession(); //세션이 있으면 세션 반환, 없으면 신규 세션을 생성하여 반환
+            session.setAttribute(SessionConstant.LOGIN_MEMBER,loginMember); //이름, 값을 인자로 세션값 바인딩
             return "redirect:/";
         }
         return "/login/login";
+    }
+
+    @PostMapping(value="/logout.do")
+    public String logout(Member member, HttpServletRequest request){
+
+        HttpSession session = request.getSession(false);
+        if(session != null){
+            session.invalidate();   //세션 날리기
+        }
+
+        return "redirect:/";
+
     }
 
 }
