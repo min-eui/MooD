8e926b8d5e46e1c4416f7a0b29c10e1c15ae68d6
jh.kim <fnqllove11@naver.com>
2022-10-14 23:06:04 +0900

    header, footer  및 css 추가

--- mood-myapp/src/main/java/mood/moodmyapp/controller/HomeController.java
+++ mood-myapp/src/main/java/mood/moodmyapp/controller/HomeController.java
@@ -1,13 +1,29 @@
 package mood.moodmyapp.controller;
 
+import mood.moodmyapp.Session.SessionConstant;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-@Controller
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home(){
+    public String home(HttpServletRequest request, Model model){
+        HttpSession session;
+        session = request.getSession(false);
+        //로그인한 상태라면
+        // 세션에 저장된 회원 조회
+        String isMember = (String)session.getAttribute(SessionConstant.LOGIN_MEMBER);
+        if(isMember.isEmpty()){
+            model.addAttribute("isMember","null");
+        }else{
+            model.addAttribute("isMember",isMember);
+        }
+
         return "index";
     }
 
